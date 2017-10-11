350c292c6639077f54ea60decc79495676664545
John Geddes <jgeddes@thoughtworks.com>
2017-10-11 17:08:58 +1000

    [JG] Removing main.rs which was a legacy of the codebase this library was taken from.

--- src/keccak.rs
+++ src/keccak.rs
@@ -1,3 +1,13 @@
+#![crate_type = "lib"]
+#![crate_name = "keccakrs"]
+
+mod constants;
+mod keccak;
+mod round;
+mod padder;
+mod sponge;
+mod keccak_f;
+
 use sponge::*;
 use padder::*;
 
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![crate_type = "lib"]
-#![crate_name = "keccakrs"]
-
-mod constants;
-mod keccak;
-mod round;
-mod padder;
-mod sponge;
-mod keccak_f;
